4281eaf9268512e6b4907b9e7e2ce0025fb35e80
rakovets <rakovets@users.noreply.github.com>
2022-08-20 03:29:10 +0000

    deploy: fc9b4c9a6198b0720659490c857feb74525b6c85

--- convert-enhorse.sh
+++ convert-enhorse.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd ./questions-answers
-find . -iname "*.md" | xargs -I '{}' basename {} .md | xargs -I '{}' kramdoc '{}.md'
+#find . -iname "*.md" | xargs -I '{}' basename {} .md | xargs -I '{}' kramdoc '{}.md'
 #rm -rf *.md
+pwd
+sed -i ':a;N;$!ba;s/\nxref\:README.adoc\[Вопросы для собеседования\]\n//g' *
+sed -i ':a;N;$!ba;s/\:doctype\: book\n//g' *
+sed -i 's/= Источники/== Источники/g' *
+#sed -i ':a;N;$!ba;s/\* <<.*,.*>>\n//g' *
+
