f1d987370ab5dec5a1f2f451691ead958cc749b2
rakovets <rakovets@users.noreply.github.com>
2022-08-21 04:28:45 +0000

    deploy: 0b3ca61e87202d9122c0b5565371515c57361a51

--- convert-enhorse.sh
+++ convert-enhorse.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 cd ./questions-answers
-#find . -iname "*.md" | xargs -I '{}' basename {} .md | xargs -I '{}' kramdoc '{}.md'
-#rm -rf *.md
-pwd
-sed -i ':a;N;$!ba;s/\nxref\:README.adoc\[Вопросы для собеседования\]\n//g' *
+find . -iname "*.md" | xargs -I '{}' basename {} .md | xargs -I '{}' kramdoc '{}.md'
+rm -rf *.md
+sed -i 's/= Источники/== Источники/g' *
 sed -i ':a;N;$!ba;s/\:doctype\: book\n//g' *
-#sed -i 's/= Источники/== Источники/g' *
+sed -i ':a;N;$!ba;s/xref:README\.adoc\[Вопросы для собеседования\]/to_delete_text/g' *
 sed -i 's/^\* <<.*,.*>>/to_delete_text/g' *
-sed -i ':a;N;$!ba;s/to_delete_text\n//g' *
 sed -i 's/^<<.*,.*>>/to_delete_text/g' *
+sed -i ':a;N;$!ba;s/to_delete_text\n//g' *
 sed -i ':a;N;$!ba;s/to_delete_text\n\n//g' *
-
+sed -i ':a;N;$!ba;s/\nto_delete_text//g' *
