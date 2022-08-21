2fee0a512a14962ad5a02de95be6c1032b53c9ac
rakovets <rakovets@users.noreply.github.com>
2022-08-21 04:05:13 +0000

    deploy: c5cb0f901eb3317e66797ebfd629a1f452280112

--- convert-enhorse.sh
+++ convert-enhorse.sh
@@ -5,6 +5,9 @@ cd ./questions-answers
 pwd
 sed -i ':a;N;$!ba;s/\nxref\:README.adoc\[Вопросы для собеседования\]\n//g' *
 sed -i ':a;N;$!ba;s/\:doctype\: book\n//g' *
-sed -i 's/= Источники/== Источники/g' *
-#sed -i ':a;N;$!ba;s/\* <<.*,.*>>\n//g' *
+#sed -i 's/= Источники/== Источники/g' *
+sed -i 's/^\* <<.*,.*>>/to_delete_text/g' *
+sed -i ':a;N;$!ba;s/to_delete_text\n//g' *
+sed -i 's/^<<.*,.*>>/to_delete_text/g' *
+sed -i ':a;N;$!ba;s/to_delete_text\n\n//g' *
 
